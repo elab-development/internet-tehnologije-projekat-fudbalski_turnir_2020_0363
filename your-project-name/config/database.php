ed6b6e6548140f5c637fc4c0f15a86ae66d105e8
BaNbO01 <bobantodic23@gmail.com>
2024-07-05 12:23:02 +0200

    Izmenjen user i database.php

--- your-project-name/config/database.php
+++ your-project-name/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -63,17 +63,25 @@ return [
             ]) : [],
         ],
 
-        'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'Knockout'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options'  => [
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin') // Auth database
-            ]
-        ],
+        // 'mongodb' => [
+        //     'driver'   => 'mongodb',
+        //     'host'     => env('DB_HOST', 'localhost'),
+        //     'port'     => env('DB_PORT', 27017),
+        //     'database' => env('DB_DATABASE', 'Knockout'),
+        //     'username' => env('DB_USERNAME'),
+        //     'password' => env('DB_PASSWORD'),
+        //     'options'  => [
+        //         'database' => env('DB_AUTHENTICATION_DATABASE', 'admin') // Auth database
+        //     ]
+        // ],
+
+     
+    'mongodb' => [
+        'driver'   => 'mongodb',
+        'dsn' => env('DB_URI', 'mongodb://localhost:27017/laravel'),
+        'database' => 'laravel',
+    ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
