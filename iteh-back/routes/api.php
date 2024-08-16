c6de79713afcff75754ade8a0c4d9c886976cb0e
BaNbO01 <bobantodic23@gmail.com>
2024-08-16 22:58:28 +0200

    Dodate CRUD operacije za rad sa timom, kao i odgovarajuce API rute

--- iteh-back/routes/api.php
+++ iteh-back/routes/api.php
@@ -26,3 +26,14 @@ Route::middleware('auth:sanctum')->group(function () {
    
 });
 
+//timovi
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('teams', [TeamController::class, 'index']);
+    Route::get('teams/{id}', [TeamController::class, 'show']);
+    Route::post('teams', [TeamController::class, 'store']);
+    Route::put('teams/{id}', [TeamController::class, 'update']);
+    Route::delete('teams/{id}', [TeamController::class, 'destroy']);
+  
+});
+
