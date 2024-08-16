4ed52b7866cab318cde9822138db805e0ec2686d
BaNbO01 <bobantodic23@gmail.com>
2024-08-16 16:40:44 +0200

    Dodate CRUD operacije za rad sa igracem, kao i odgovarajuce API rute

--- iteh-back/routes/api.php
+++ iteh-back/routes/api.php
@@ -13,3 +13,16 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 
+use App\Http\Controllers\PlayerController;
+
+//igraci
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('players', [PlayerController::class, 'index']);
+    Route::get('players/{id}', [PlayerController::class, 'show']);
+    Route::post('players', [PlayerController::class, 'store']);
+    Route::put('players/{id}', [PlayerController::class, 'update']);
+    Route::delete('players/{id}', [PlayerController::class, 'destroy']);
+   
+});
+
