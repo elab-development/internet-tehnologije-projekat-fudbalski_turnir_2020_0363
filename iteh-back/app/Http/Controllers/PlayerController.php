48c6ef191a17a42b8139d1d53e8cefda42d05aad
DarkoSerafinovski <darkoserafinovski@gmail.com>
2024-08-21 01:55:45 +0200

    Implementirana logika za prikaz igraca

--- iteh-back/app/Http/Controllers/PlayerController.php
+++ iteh-back/app/Http/Controllers/PlayerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Player;
 use Illuminate\Http\Request;
 use App\Http\Resources\PlayerResource;
+use Illuminate\Support\Facades\Log;
 class PlayerController extends Controller
 {
     public function index()
@@ -71,6 +72,39 @@ class PlayerController extends Controller
         }
         
     }
+    public function getAllPlayersWithAllStatistics(Request $request)
+{
+    try {
+       
+        $perPage = $request->input('perPage', 20);
+
+       
+        $players = Player::paginate($perPage);
+
+ 
+        foreach ($players as $player) {
+            $stats = $player->stats_player;
+
+            $cumulativeStats = [
+                'broj_golova' => $stats->sum('broj_golova'),
+                'broj_asistencija' => $stats->sum('broj_asistencija'),
+                'broj_zutih_kartona' => $stats->sum('broj_zutih_kartona'),
+                'broj_crvenih_kartona' => $stats->sum('broj_crvenih_kartona'),
+                'broj_suta_u_okvir' => $stats->sum('broj_suta_u_ovkir'),
+                'broj_suta_van_okvira' => $stats->sum('broj_suta_van_okvira'),
+            ];
+
+      
+            $player->cumulative_stats = $cumulativeStats;
+        }
+
+      
+        return PlayerResource::collection($players);
+    } catch (\Exception $e) {
+        \Log::error($e->getMessage());
+        return response()->json(['error' => 'Greska u citanju podataka'], 500);
+    }
+}
 
    
 
--- iteh-back/routes/api.php
+++ iteh-back/routes/api.php
@@ -2,60 +2,75 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PlayerController;
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
- 
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 
-use App\Http\Controllers\PlayerController;
-
 //igraci
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('players/cumulative_stats', [PlayerController::class, 'getAllPlayersWithAllStatistics']);
     Route::get('players', [PlayerController::class, 'index']);
     Route::get('players/{id}', [PlayerController::class, 'show']);
-    Route::post('players', [PlayerController::class, 'store']);
-    Route::put('players/{id}', [PlayerController::class, 'update']);
-    Route::delete('players/{id}', [PlayerController::class, 'destroy']);
-   
+    Route::middleware('role:admin')->group(function () {
+        Route::post('players', [PlayerController::class, 'store']);
+        Route::put('players/{id}', [PlayerController::class, 'update']);
+        Route::delete('players/{id}', [PlayerController::class, 'destroy']);
+    });
 });
 
-//timovi
-
 use App\Http\Controllers\TeamController;
+
+//teams
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('teams', [TeamController::class, 'index']);
     Route::get('teams/{id}', [TeamController::class, 'show']);
-    Route::post('teams', [TeamController::class, 'store']);
-    Route::put('teams/{id}', [TeamController::class, 'update']);
-    Route::delete('teams/{id}', [TeamController::class, 'destroy']);
-  
+    
+    Route::middleware('role:admin')->group(function () {
+        Route::post('teams', [TeamController::class, 'store']);
+        Route::put('teams/{id}', [TeamController::class, 'update']);
+        Route::delete('teams/{id}', [TeamController::class, 'destroy']);
+    });
 });
-
-
 
 use App\Http\Controllers\GameController;
 
-//utakmice
+//games
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('games', [GameController::class, 'index']);
     Route::get('games/{id}', [GameController::class, 'show']);
-    Route::post('games', [GameController::class, 'store']);
-    Route::put('games/{id}', [GameController::class, 'update']);
-    Route::delete('games/{id}', [GameController::class, 'destroy']);
+     
+    Route::middleware('role:admin')->group(function () {   
+        Route::post('games', [GameController::class, 'store']);
+        Route::put('games/{id}', [GameController::class, 'update']);
+        Route::delete('games/{id}', [GameController::class, 'destroy']);
+    });
 });
 
-
 use App\Http\Controllers\TournamentController;
-//turniri
+
+//tournaments
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('tournaments', TournamentController::class);
+    Route::apiResource('tournaments', TournamentController::class)->only([
+        'index', 'show'
+    ]);
+   
+
+    Route::middleware('role:admin')->group(function () {
+        Route::apiResource('tournaments', TournamentController::class)->only([
+            'store', 'update', 'destroy'
+        ]);
+    });
 });
