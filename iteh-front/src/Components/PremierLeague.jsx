6c7aeb6da4af338da21205daa5d6f4cc8df72ec2
DarkoSerafinovski <darkoserafinovski@gmail.com>
2024-08-26 12:16:23 +0200

    Kreiranje stranice za prikaz tabele premijer lige public api

---
+++ iteh-front/src/Components/PremierLeague.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NavigationButtons from './NavigationButtons';
+import NavigationMenu from './NavigationMenu';
+import './PremierLeague.css';
+
+const PremierLeague = () => {
+  const [standings, setStandings] = useState([]);
+  const [season, setSeason] = useState(2024); 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/v4/competitions/PL/standings/?season=${season}`, {
+          headers: { 'X-Auth-Token': '3f67e6c5848344f99e46142768ce07fa' }
+        });
+        setStandings(response.data.standings[0].table);
+      } catch (error) {
+        console.error("Error fetching the data", error);
+      }
+    };
+
+    fetchData();
+  }, [season]); 
+
+  const handleSeasonChange = (event) => {
+    setSeason(event.target.value);
+  };
+
+  return (
+    <div className="PremierLeague">
+       <NavigationMenu role={window.sessionStorage.getItem("role")}/>
+      <h1>Premier League Standings</h1>
+      <NavigationButtons />
+      <label htmlFor="season">Select Season: </label>
+      <select id="season" value={season} onChange={handleSeasonChange}>
+        {Array.from({ length: 2024 - 1981 + 1 }, (_, i) => 1981 + i).map(year => (
+          <option key={year} value={year}>{year}</option>
+        ))}
+      </select>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Team</th>
+            <th>Played</th>
+            <th>Won</th>
+            <th>Drawn</th>
+            <th>Lost</th>
+            <th>Goals For</th>
+            <th>Goals Against</th>
+            <th>Goal Difference</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {standings.map((team) => (
+            <tr key={team.team.id}>
+              <td>{team.position}</td>
+              <td>
+                <img src={team.team.crest} alt={team.team.name} style={{ width: '30px', marginRight: '10px' }} />
+                {team.team.name}
+              </td>
+              <td>{team.playedGames}</td>
+              <td>{team.won}</td>
+              <td>{team.draw}</td>
+              <td>{team.lost}</td>
+              <td>{team.goalsFor}</td>
+              <td>{team.goalsAgainst}</td>
+              <td>{team.goalDifference}</td>
+              <td>{team.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default PremierLeague;
